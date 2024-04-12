68317575f4fd6891bcc55c8a85b37ef6a2da41e6
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-04-12 12:47:30 +0600

    Dynamic title and navbar added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,11 @@
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div className="">
+            <Helmet>
+                <title>Home | Dream Land</title>
+            </Helmet>
             <h2>This is home</h2>
         </div>
     );
